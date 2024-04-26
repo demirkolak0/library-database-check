69b801b31df42bfd6b171d367dec12072b00ff73
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2024-04-26 15:36:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,39 @@
 #include<stdio.h>
+void addingbook();
+void readingbooks();
+FILE *fptr;
+char added_book[100];
+char DatabaseRead[999];
 int main()
 {
-    FILE *fptr;
     int add_code;
-    char added_book[100];
-    char DatabaseRead[999];
     printf("Welcome back!! , How can I help you today? \n The options are adding a book or reading the database,\n If you wish to add a book please enter 1 and press enter. \n You can read the database by enter 2 and press enter. \n Choice:");
     scanf("%i" , &add_code);
     getchar();
     if (add_code == 1)
     {
+        addingbook();
+    } else if (add_code == 2)
+    {
+        readingbooks();
+    }
+    return 0;
+}
+void addingbook()
+{
         printf("Please enter the name of the book: ");
         fgets(added_book,100,stdin);
         fptr = fopen("lib-db.sdkapdb" , "a");
         fprintf(fptr , "%s" , added_book);
         printf("Book added succesfully!!");
         fclose(fptr);
-    } else if (add_code == 2)
-    {
+}
+void readingbooks()
+{
         printf("Reading the database.");
         fptr = fopen("lib-db.sdkapdb" , "r");
         while(fgets(DatabaseRead, 999, fptr)) {
-            printf("%s", DatabaseRead);
+        printf("%s", DatabaseRead);
         }
         fclose(fptr);
-    }
-    return 0;
 }
